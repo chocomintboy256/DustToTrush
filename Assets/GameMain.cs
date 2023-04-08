c8871f6d74508c1618a0d487d63c09354c5b2698
chocomintboy256 <rose.coffee0224@gmail.com>
2023-04-08 18:10:13 +0900

    時間制限追加 60~0

--- Assets/GameMain.cs
+++ Assets/GameMain.cs
@@ -1,28 +1,44 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameMain : MonoBehaviour
 {
     GameObject go;
     public GameObject box;
     public Text scoreText;
+    public Text timeText;
     int totalScore = 0;
-
+    int timeRemining = 60;
+    
     // Start is called before the first frame update
     void Start()
     {
         scoreText.text = "Score: " + totalScore.ToString();
         GameObject.FindWithTag("Respawn").GetComponent<Dust>().ResetPosition();
+
+        // get hierarchy objects
+        Scene scene = SceneManager.GetSceneByBuildIndex(0);
+        GameObject[] rootObjects = scene.GetRootGameObjects();
+        foreach (GameObject obj in rootObjects)
+        {
+            Debug.Log(obj.name);
+        }
+        TimerUpdate();
     }
 
     // Update is called once per frame
     void Update()
     {
-
     }
-
+    public void TimerUpdate()
+    {
+        timeRemining--;
+        timeText.text = "Time: " + timeRemining;
+        Invoke("TimerUpdate", 1.0f);
+    }
     public void TrushIn(int trushScore)
     {
         scoreText.text = "Score: " + (totalScore += trushScore).ToString();
