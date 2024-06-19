d4b199d14db280bc3b48aa3f2732fb5932c5a591
chocomintboy256 <privateiwai@gmail.com>
2024-06-20 04:16:52 +0900

    ちょっと強いレアなゴミを追加しました

---
+++ Assets/Script/DustType2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+public class DustType2 : DragCharactor
+{
+    private GameObject box;
+    public bool BonusFg { get; protected set; }
+    private enum Costume { Bonus };
+
+    // 生成直後
+    void Awake()
+    {
+        dustManager = transform.parent.GetComponent<DustManager>();
+        BonusFg = false;
+
+        MaxHP = 30;
+        HP = MaxHP;
+        atk = 10;
+    }
+ 
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        target = GameMain.ins.boxs[0];
+        Score = 20;
+        dustManager = transform.parent.GetComponent<DustManager>();
+    }
+
+    // Update is called once per frame
+    // protected virtual void Update() { }
+}
