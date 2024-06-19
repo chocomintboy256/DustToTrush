c131cf4cdcb025aff0e0494ada67a3b88f099890
chocomintboy256 <privateiwai@gmail.com>
2024-06-20 04:10:48 +0900

    階段を追加

---
+++ Assets/Script/StepDown.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+public class StepDown : DragCharactor
+{
+    private GameObject box;
+    public bool BonusFg { get; protected set; }
+    private enum Costume { Bonus };
+    public GameObject Target { get { return target; } set { target = value; } }
+
+    // 生成直後
+    void Awake()
+    {
+        BonusFg = false;
+
+        HP = 0;
+        MaxHP = 0;
+        atk = 0;
+    }
+ 
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        Score = 0;
+    }
+    // Update is called once per frame
+    // protected virtual void Update() { }
+
+    // 生成
+    public static DragCharactor Generate(Vector3 position) { 
+        GameObject stepDown = Instantiate(GameMain.ins.MasterStepDown, position, Quaternion.identity, GameMain.ins.transform.parent);
+        GameMain.ins.stepDown = stepDown.GetComponent<StepDown>();
+        GameMain.ins.stepDown.Target = GameMain.ins.boxs[0];
+        return stepDown.GetComponent<DragCharactor>();
+    }
+
+}
---
+++ Assets/Script/StepUp.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+using UnityEngine.UIElements;
+
+public class StepUp : DragCharactor
+{
+    private GameObject box;
+    public bool BonusFg { get; protected set; }
+    private enum Costume { Bonus };
+    public GameObject Target { get { return target; } set { target = value; } }
+
+    // 生成直後
+    void Awake()
+    {
+        BonusFg = false;
+
+        HP = 0;
+        MaxHP = 0;
+        atk = 0;
+    }
+ 
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        Score = 0;
+    }
+    // Update is called once per frame
+    // protected virtual void Update() { }
+
+    // 生成
+    public static DragCharactor Generate(Vector3 position) { 
+        GameObject stepUp = Instantiate(GameMain.ins.MasterStepUp, position, Quaternion.identity, GameMain.ins.transform.parent);
+        GameMain.ins.stepUp = stepUp.GetComponent<StepUp>();
+        GameMain.ins.stepUp.Target = GameMain.ins.boxs[0];
+        return stepUp.GetComponent<DragCharactor>();
+    }
+}
